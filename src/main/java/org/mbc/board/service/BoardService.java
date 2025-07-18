436744639fd9f5c0deb958050f78688b7a6daec9
220-15\dlgksmf0304 <dlgksmf0304@gmail.com>
2025-07-18 15:47:18 +0900

    ControllerList 완료

--- src/main/java/org/mbc/board/service/BoardService.java
+++ src/main/java/org/mbc/board/service/BoardService.java
@@ -1,6 +1,8 @@
 package org.mbc.board.service;
 
 import org.mbc.board.dto.BoardDTO;
+import org.mbc.board.dto.PageRequestDTO;
+import org.mbc.board.dto.PageResponseDTO;
 
 public interface BoardService {
     // 조장용 코드 -> 시그니처만 필요 -> Impl 구현클래스 -> 실행문을 만든다.
@@ -13,6 +15,9 @@ public interface BoardService {
     void modify(BoardDTO boardDTO); // 프론트에서 dto가 넘엉면 수정작업
 
     void remove(Long bno); // 프론트에서 bno가 넘어오면 삭제 작업 진행
+
+    PageResponseDTO<BoardDTO> list(PageRequestDTO pageRequestDTO);
+    // 페이징처리에 대한 요청을 리스트로 처리하고 결과를 응답으로 보내는 메서드
 
 
 }
--- src/test/java/org/mbc/board/service/BoardServiceTests.java
+++ src/test/java/org/mbc/board/service/BoardServiceTests.java
@@ -3,6 +3,8 @@ package org.mbc.board.service;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.mbc.board.dto.BoardDTO;
+import org.mbc.board.dto.PageRequestDTO;
+import org.mbc.board.dto.PageResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -39,6 +41,47 @@ public class BoardServiceTests {
                 .content("서비스에서 수정된 내용")
                 .build();
         boardService.modify(boardDTO); // 프론트에서 객체가 넘어가 수정이 되었는지 테스트
-    }
+    } // testModify 메서드 종료
+
+    @Test
+    public void testList(){
+        // 프론트에서 넘어오는 데이터를 이용해서 페이징과 검색과 정렬처리용
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .type("tcw") // 제목, 내용, 작성자
+                .keyword("1") // 1을 찾는다.
+                .page(1) // 현재페이지는 1
+                .size(10) // 10개씩 보여달라
+                .build();
+
+        PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
+
+        log.info(responseDTO);
+
+        // Hibernate:
+        //    select
+        //        b1_0.bno,
+        //        b1_0.content,
+        //        b1_0.moddate,
+        //        b1_0.regdate,
+        //        b1_0.title,
+        //        b1_0.writer
+        //    from
+        //        board b1_0
+        //    where
+        //        b1_0.bno>?
+        //    order by
+        //        b1_0.bno desc
+        //    limit
+        //        ?, ?
+        //Hibernate:
+        //    select
+        //        count(b1_0.bno)
+        //    from
+        //        board b1_0
+        //    where
+        //        b1_0.bno>?
+
+    } // testList 메서드 종료
+
 
 }
